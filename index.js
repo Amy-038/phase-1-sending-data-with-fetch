365415828b5e1efdf89317bda533bd61628c1758
Amy-038 <amy.sirma@student.moringaschool.com>
2024-01-11 17:35:38 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// Add your code here
+function submitData(name, email){
+    return fetch('http://localhost:3000/users', {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            'name': name,
+            'email': email,
+        })
+    })
+    .then(resp => resp.json())
+    .then(object => {
+        document.querySelector('body').innerHTML = object.id
+    })
+    .catch((error) => {
+        document.querySelector('body').innerHTML = error.message
+    })
+}
+
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let name = e.target.userName.value;
+    let email = e.target.userEmail.value;
+    submitData(name, email)
+;})
